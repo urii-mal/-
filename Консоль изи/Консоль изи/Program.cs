e50f9e6f4b5464a659f13733abb2c01d30cac721
Lisai1 <dashuta.babikovalist.ru@gmail.com>
2026-02-02 18:49:44 +0600

    Update Program.cs
    Добавлено второе число

--- Консоль изи/Консоль изи/Program.cs
+++ Консоль изи/Консоль изи/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Введите первое число:");
 int a = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
 Console.WriteLine(a);
+Console.WriteLine(b);
